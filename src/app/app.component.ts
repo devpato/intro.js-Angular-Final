a781b5a994b12fa328b93f24485b64dba5fcab1b
Patricio Vargas <pevargas2730@gmail.com>
2019-09-15 02:00:35 -0500

    showing steps of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import introJS from 'intro.js/intro.js';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +6,35 @@ import introJS from 'intro.js/intro.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  introJS = introJs();
+  constructor() {
+    this.introJS.setOptions({
+      steps: [
+        {
+          element: '#step1',
+          intro: 'Welcome to your new app!',
+          position: 'bottom'
+        },
+        {
+          element: '#step2',
+          intro: "Ok, wasn't that fun?",
+          position: 'right'
+        },
+        {
+          element: '#step3',
+          intro: "let's keep going",
+          position: 'top'
+        },
+        {
+          element: '#step4',
+          intro: 'More features, more fun.',
+          position: 'right'
+        }
+      ]
+    });
+  }
+
   ngOnInit() {
-    introJS().start();
+    this.introJS.start();
   }
 }
