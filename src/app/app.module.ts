ee4e1eb35c532f87dc9ec86a22c9c19ce4300f05
Patricio Vargas <pevargas2730@gmail.com>
2019-09-15 00:53:35 -0500

    sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
