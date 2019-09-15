3a1108dfbcfe242b2c8ede7bdbf4bc9cbd1d6b91
Patricio Vargas <pevargas2730@gmail.com>
2019-09-15 01:22:34 -0500

    cleaned code

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import introJS from 'intro.js/intro.js';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
 export class TitleComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    introJS().start();
   }
-
 }
