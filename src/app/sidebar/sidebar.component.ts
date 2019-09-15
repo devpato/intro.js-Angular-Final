de3232cec8d496a4aa7c93421244483e46b06599
Patricio Vargas <pevargas2730@gmail.com>
2019-09-15 02:07:17 -0500

    created steps on sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  introJS = introJs();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit() {
+  startSteps(): void {
+    this.introJS
+      .setOptions({
+        steps: [
+          {
+            element: '#step1-li',
+            intro: 'Welcome to your new app!',
+            position: 'bottom'
+          },
+          {
+            element: '#step2-li',
+            intro: "Ok, wasn't that fun?",
+            position: 'right'
+          },
+          {
+            element: '#step3-li',
+            intro: "let's keep going",
+            position: 'top'
+          },
+          {
+            element: '#step4-li',
+            intro: 'More features, more fun.',
+            position: 'right'
+          },
+          {
+            //As you can see, thanks to the element ID I can set a step in an element in an other component
+            element: '#step1',
+            intro: 'Accessed and element in another component',
+            position: 'right'
+          }
+        ]
+      })
+      .start();
   }
-
 }
