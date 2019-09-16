f68a37011ab307856a80fb3eae8daee54031ca1a
Patricio Vargas <pevargas2730@gmail.com>
2019-09-15 21:24:40 -0500

    FINAL

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -17,30 +17,25 @@ export class SidebarComponent implements OnInit {
         steps: [
           {
             element: '#step1-li',
-            intro: 'Welcome to your new app!',
-            position: 'bottom'
+            intro: 'Welcome to your new app!'
           },
           {
             element: '#step2-li',
-            intro: "Ok, wasn't that fun?",
-            position: 'right'
+            intro: "Ok, wasn't that fun?"
           },
           {
             element: '#step3-li',
-            intro: "let's keep going",
-            position: 'top'
+            intro: "let's keep going"
           },
           {
             element: '#step4-li',
-            intro: 'More features, more fun.',
-            position: 'right'
+            intro: 'More features, more fun.'
           },
           {
             // As you can see, thanks to the element ID
             // I can set a step in an element in an other component
             element: '#step1',
-            intro: 'Accessed and element in another component',
-            position: 'right'
+            intro: 'Accessed and element in another component'
           }
         ],
         hidePrev: true,
